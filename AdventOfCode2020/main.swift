ac1955108f891d008a91bc1579a665fa2f6df0b2
alok <alokkrnk@gmail.com>
2020-12-01 16:54:19 +0530

    Initial Commit

---
+++ AdventOfCode2020/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode2020
+//
+//  Created by Alok Karnik on 01/12/20.
+//  Copyright © 2020 Alok Karnik. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
